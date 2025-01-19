d79fc6ff4a5915de9269d8c4b933b9035e4a2ff0
Alexander Elfimow <my.free.mail@gmx.de>
2025-01-19 17:21:45 +0100

    formula1, fp64: added file

---
+++ formula1/fp64/lhs.cpp
@@ -0,0 +1,14 @@
+#include "fp64.h"
+#include "lhs.h"
+
+#include <cmath>
+
+fp64 lhs(fp64 x)
+{
+    fp64 const x1 = sin(x);
+    fp64 const x2 = cos(x);
+
+    fp64 const result = (x1 * x1) + (x2 * x2);
+
+    return result;
+}
