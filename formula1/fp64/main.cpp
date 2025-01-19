c4a3853748332e94bf32390f24cb36be853005f1
Alexander Elfimow <my.free.mail@gmx.de>
2025-01-19 17:21:47 +0100

    formula1, fp64: added file

---
+++ formula1/fp64/main.cpp
@@ -0,0 +1,51 @@
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <numbers>
+
+#include "fp64.h"
+#include "lhs.h"
+#include "rhs.h"
+#include "macheps.h"
+
+int main(int, char *[])
+{
+    fp64 const epsilon = macheps();
+
+    fp64 const delta_x = (2.0 * std::numbers::pi_v<fp64>);
+
+    int const denominator = 1000;
+
+    for (int nominator = -denominator; nominator <= denominator; ++nominator)
+    {
+        fp64 const k = (static_cast<fp64>(nominator) / static_cast<fp64>(denominator));
+
+        fp64 const x = (k * delta_x);
+
+        fp64 const lhs_value = lhs(x);
+        fp64 const rhs_value = rhs();
+
+        fp64 const diff = (lhs_value - rhs_value);
+
+        bool const failed = (diff >= epsilon);
+
+        if (failed)
+        {
+            std::cout
+                << std::setprecision(10)
+                << "Check failed for (fp64): x = " << x
+                << "; diff = " << diff
+                << "; " << nominator << "/" << denominator
+                << std::endl;
+        }
+    }
+
+    std::cout
+        << std::setprecision(10)
+        << "Epsilon (fp64): "
+        << epsilon
+        << std::endl;
+
+    return EXIT_SUCCESS;
+}
