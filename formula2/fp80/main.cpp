fe57d550e03bb4d9b5100ea29b5c85aa6768735c
Alexander Elfimow <my.free.mail@gmx.de>
2025-01-31 20:21:34 +0100

    formula2, fp80: added file

---
+++ formula2/fp80/main.cpp
@@ -0,0 +1,51 @@
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <numbers>
+
+#include "fp80.h"
+#include "lhs.h"
+#include "rhs.h"
+#include "macheps.h"
+
+int main(int, char *[])
+{
+    fp80 const epsilon = macheps();
+
+    fp80 const delta_x = (2.0L * std::numbers::pi_v<fp80>);
+
+    int const denominator = 1000;
+
+    for (int nominator = -denominator; nominator <= denominator; ++nominator)
+    {
+        fp80 const k = (static_cast<fp80>(nominator) / static_cast<fp80>(denominator));
+
+        fp80 const x = (k * delta_x);
+
+        fp80 const lhs_value = lhs(x);
+        fp80 const rhs_value = rhs(x);
+
+        fp80 const diff = (lhs_value - rhs_value);
+
+        bool const failed = (diff >= epsilon);
+
+        if (failed)
+        {
+            std::cout
+                << std::setprecision(10)
+                << "Check failed for (fp80): x = " << x
+                << "; diff = " << diff
+                << "; " << nominator << "/" << denominator
+                << std::endl;
+        }
+    }
+
+    std::cout
+        << std::setprecision(10)
+        << "Epsilon (fp80): "
+        << epsilon
+        << std::endl;
+
+    return EXIT_SUCCESS;
+}
