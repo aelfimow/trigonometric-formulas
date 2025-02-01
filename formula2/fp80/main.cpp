8a8ea33443f05b083da0553dd1a47225a410c55e
Alexander Elfimow <my.free.mail@gmx.de>
2025-02-01 21:06:48 +0100

    formula2, fp80: fabs

--- formula2/fp80/main.cpp
+++ formula2/fp80/main.cpp
@@ -1,13 +1,15 @@
+#include "fp80.h"
+#include "lhs.h"
+#include "rhs.h"
+#include "macheps.h"
+
 #include <cstdlib>
 #include <iostream>
 #include <iomanip>
 #include <vector>
 #include <numbers>
+#include <cmath>
 
-#include "fp80.h"
-#include "lhs.h"
-#include "rhs.h"
-#include "macheps.h"
 
 int main(int, char *[])
 {
@@ -26,7 +28,7 @@ int main(int, char *[])
         fp80 const lhs_value = lhs(x);
         fp80 const rhs_value = rhs(x);
 
-        fp80 const diff = (lhs_value - rhs_value);
+        fp80 const diff = fabsl(lhs_value - rhs_value);
 
         bool const failed = (diff >= epsilon);
 
