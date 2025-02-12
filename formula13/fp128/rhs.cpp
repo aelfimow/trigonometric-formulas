baa1c08fddfbe12e5f5fc6999ca5958368736147
Alexander Elfimow <my.free.mail@gmx.de>
2025-02-12 16:55:13 +0100

    formula13, fp128: added file

---
+++ formula13/fp128/rhs.cpp
@@ -0,0 +1,14 @@
+#include "fp128.h"
+#include "rhs.h"
+#include "cotq.h"
+
+
+fp128 rhs(fp128 alpha, fp128 beta)
+{
+    fp128 const a = cotq(alpha);
+    fp128 const b = cotq(beta);
+
+    fp128 const result = ((a * b) + 1.0Q) / (b - a);
+
+    return result;
+}
