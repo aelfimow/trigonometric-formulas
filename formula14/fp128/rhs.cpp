aee581e6f22288099bf865d527f13fe5d67ec773
Alexander Elfimow <my.free.mail@gmx.de>
2025-02-13 19:51:01 +0100

    formula14, fp128: added file

---
+++ formula14/fp128/rhs.cpp
@@ -0,0 +1,14 @@
+#include "fp128.h"
+#include "rhs.h"
+
+#include <quadmath.h>
+
+fp128 rhs(fp128 alpha, fp128 beta)
+{
+    fp128 const a = cosq(beta);
+    fp128 const b = cosq(alpha);
+
+    fp128 const result = (a * a) - (b * b);
+
+    return result;
+}
