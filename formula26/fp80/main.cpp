60e3232c991efd86f7d7357594b82a49d9b62baa
Alexander Elfimow <my.free.mail@gmx.de>
2025-02-22 20:18:45 +0100

    formula26, fp80: added digits for setprecision

--- formula26/fp80/main.cpp
+++ formula26/fp80/main.cpp
@@ -13,6 +13,8 @@
 
 int main(int, char *[])
 {
+    int const digits = 21;
+
     fp80 const epsilon = macheps();
 
     fp80 const delta_x = (2.0L * std::numbers::pi_v<fp80>);
@@ -40,7 +42,7 @@ int main(int, char *[])
         if (failed)
         {
             std::cout
-                << std::setprecision(10)
+                << std::setprecision(digits)
                 << "Check failed for (fp80): x = " << x
                 << "; diff = " << diff
                 << "; " << nominator << "/" << denominator
@@ -49,7 +51,7 @@ int main(int, char *[])
     }
 
     std::cout
-        << std::setprecision(10)
+        << std::setprecision(digits)
         << "Epsilon (fp80): "
         << epsilon
         << std::endl;
