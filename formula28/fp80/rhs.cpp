3615bcf8bdd8656afde2853462cdbeb70b61e8df
Alexander Elfimow <my.free.mail@gmx.de>
2025-02-25 19:34:14 +0100

    formula28, fp80: added file

---
+++ formula28/fp80/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp80.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     1 - 2\cdot\sin^{2}{\alpha}
+ * @f]
+ */
+fp80 rhs(fp80 x)
+{
+    fp80 const a = sinl(x);
+
+    fp80 const result = 1.0L - (2.0L * (a * a));
+
+    return result;
+}
