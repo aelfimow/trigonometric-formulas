7bb49627a84fb5b16699b25ef1c633e0ea3b36e7
Alexander Elfimow <my.free.mail@gmx.de>
2025-02-25 19:49:17 +0100

    formula29, fp80: added file

---
+++ formula29/fp80/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp80.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     2\cdot\cos^{2}{\alpha} - 1
+ * @f]
+ */
+fp80 rhs(fp80 x)
+{
+    fp80 const a = cosl(x);
+
+    fp80 const result = (2.0L * (a * a)) - 1.0L;
+
+    return result;
+}
