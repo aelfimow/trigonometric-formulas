5d285edfe35fb39d88d51770972b9b0102458281
Alexander Elfimow <my.free.mail@gmx.de>
2025-02-25 19:49:00 +0100

    formula29, fp128: added file

---
+++ formula29/fp128/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp128.h"
+#include "rhs.h"
+
+#include <quadmath.h>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     2\cdot\cos^{2}{\alpha} - 1
+ * @f]
+ */
+fp128 rhs(fp128 x)
+{
+    fp128 const a = cosq(x);
+
+    fp128 const result = (2.0Q * (a * a)) - 1.0Q;
+
+    return result;
+}
