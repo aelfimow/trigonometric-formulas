848a17017dce0868721571c999da94e3ac6ce2f4
Alexander Elfimow <my.free.mail@gmx.de>
2025-02-26 20:00:18 +0100

    formula30, fp32: added file

---
+++ formula30/fp32/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp32.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     frac{2\cdot\tan^{2}{\alpha}}{1 - \tan^{2}{\alpha}}
+ * @f]
+ */
+fp32 rhs(fp32 x)
+{
+    fp32 const a = tanf(x);
+
+    fp32 const result = (2.0f * a) / (1.0f - (a * a));
+
+    return result;
+}
