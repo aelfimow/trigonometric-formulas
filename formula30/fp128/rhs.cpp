8e39fbeb43d4089068eb2bf8a9d49c5b017ab2e6
Alexander Elfimow <my.free.mail@gmx.de>
2025-02-26 20:00:15 +0100

    formula30, fp128: added file

---
+++ formula30/fp128/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp128.h"
+#include "rhs.h"
+
+#include <quadmath.h>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     frac{2\cdot\tan^{2}{\alpha}}{1 - \tan^{2}{\alpha}}
+ * @f]
+ */
+fp128 rhs(fp128 x)
+{
+    fp128 const a = tanq(x);
+
+    fp128 const result = (2.0Q * a) / (1.0Q - (a * a));
+
+    return result;
+}
