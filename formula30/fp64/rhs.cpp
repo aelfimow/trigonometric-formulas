d330760a001bb9e83694f1a968c37fad2b0f5d5d
Alexander Elfimow <my.free.mail@gmx.de>
2025-02-26 20:00:20 +0100

    formula30, fp64: added file

---
+++ formula30/fp64/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp64.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     frac{2\cdot\tan^{2}{\alpha}}{1 - \tan^{2}{\alpha}}
+ * @f]
+ */
+fp64 rhs(fp64 x)
+{
+    fp64 const a = tan(x);
+
+    fp64 const result = (2.0 * a) / (1.0 - (a * a));
+
+    return result;
+}
