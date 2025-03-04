ad6e110de3857ea9f2c5f3a9382a7ea2ee52e166
Alexander Elfimow <my.free.mail@gmx.de>
2025-03-04 19:09:41 +0100

    formula36, fp64: added file

---
+++ formula36/fp64/rhs.cpp
@@ -0,0 +1,22 @@
+#include "fp64.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     1 - 2\cdot\sin^{2}{\frac{\alpha}{2}}
+ * @f]
+ */
+fp64 rhs(fp64 x)
+{
+    fp64 const x_2 = (x / 2.0);
+
+    fp64 const a = sin(x_2);
+
+    fp64 const result = 1.0 - (2.0 * (a * a));
+
+    return result;
+}
