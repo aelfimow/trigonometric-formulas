2c17aee3cf8b84802b943ba3688690ba3f313613
Alexander Elfimow <my.free.mail@gmx.de>
2025-03-04 19:10:09 +0100

    formula36, fp80: added file

---
+++ formula36/fp80/rhs.cpp
@@ -0,0 +1,22 @@
+#include "fp80.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     1 - 2\cdot\sin^{2}{\frac{\alpha}{2}}
+ * @f]
+ */
+fp80 rhs(fp80 x)
+{
+    fp80 const x_2 = (x / 2.0L);
+
+    fp80 const a = sinl(x_2);
+
+    fp80 const result = 1.0L - (2.0L * (a * a));
+
+    return result;
+}
