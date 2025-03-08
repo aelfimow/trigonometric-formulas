bf80dc1f9eaa7c3cfcf40e77a2c245c3ca74cc79
Alexander Elfimow <my.free.mail@gmx.de>
2025-03-08 20:40:41 +0100

    formula40, fp80: added file

---
+++ formula40/fp80/lhs.cpp
@@ -0,0 +1,18 @@
+#include "fp80.h"
+#include "lhs.h"
+#include "cotl.h"
+
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \cot{\alpha}
+ * @f]
+ */
+fp80 lhs(fp80 x)
+{
+    fp80 const result = cotl(x);
+
+    return result;
+}
