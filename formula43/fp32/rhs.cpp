6fdeb86bbfa565f0f57a3dc15551b689cb70200a
Alexander Elfimow <my.free.mail@gmx.de>
2025-03-14 13:34:07 +0100

    formula43, fp32: added file

---
+++ formula43/fp32/rhs.cpp
@@ -0,0 +1,18 @@
+#include "fp32.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \sqrt{\frac{1 + \cos{\alpha}}{2}}
+ * @f]
+ */
+fp32 rhs(fp32 x)
+{
+    fp32 const result = sqrtf((1.0f + cosf(x)) / 2.0f);
+
+    return result;
+}
