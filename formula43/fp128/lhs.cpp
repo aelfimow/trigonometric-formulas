5f1d6e18b902741f09829578f341b9d0ed9efd6a
Alexander Elfimow <my.free.mail@gmx.de>
2025-03-14 13:16:44 +0100

    formula43, fp128: added file

---
+++ formula43/fp128/lhs.cpp
@@ -0,0 +1,20 @@
+#include "fp128.h"
+#include "lhs.h"
+
+#include <quadmath.h>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \cos{\frac{\alpha}{2}}
+ * @f]
+ */
+fp128 lhs(fp128 x)
+{
+    fp128 const x_2 = (x / 2.0Q);
+
+    fp128 const result = cosq(x_2);
+
+    return result;
+}
