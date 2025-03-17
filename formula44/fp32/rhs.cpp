ef19ed2521f49238a9def73680e17fcd53f49aaf
Alexander Elfimow <my.free.mail@gmx.de>
2025-03-17 20:20:41 +0100

    formula44, fp32: added file

---
+++ formula44/fp32/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp32.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \tan{\frac{\alpha}{2}} = \sqrt{\frac{1 - \cos{\alpha}}{1 + \cos{\alpha}}}
+ * @f]
+ */
+fp32 rhs(fp32 x)
+{
+    fp32 const a = cosf(x);
+
+    fp32 const result = sqrtf((1.0f - a) / (1.0f + a));
+
+    return result;
+}
