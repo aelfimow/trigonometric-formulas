0842a5d4c4bb3ab773c25ee9059ca22a15f0ad80
Alexander Elfimow <my.free.mail@gmx.de>
2025-03-17 20:20:44 +0100

    formula44, fp80: added file

---
+++ formula44/fp80/lhs.cpp
@@ -0,0 +1,20 @@
+#include "fp80.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \tan{\frac{\alpha}{2}}
+ * @f]
+ */
+fp80 lhs(fp80 x)
+{
+    fp80 const x_2 = (x / 2.0L);
+
+    fp80 const result = tanl(x_2);
+
+    return result;
+}
