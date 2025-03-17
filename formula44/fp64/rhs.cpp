355aa86ec5c0b4c325f1d66feeacde576aeefb3f
Alexander Elfimow <my.free.mail@gmx.de>
2025-03-17 20:20:43 +0100

    formula44, fp64: added file

---
+++ formula44/fp64/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp64.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \tan{\frac{\alpha}{2}} = \sqrt{\frac{1 - \cos{\alpha}}{1 + \cos{\alpha}}}
+ * @f]
+ */
+fp64 rhs(fp64 x)
+{
+    fp64 const a = cos(x);
+
+    fp64 const result = sqrt((1.0 - a) / (1.0 + a));
+
+    return result;
+}
