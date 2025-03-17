4ba198203d79f19fb96e6b116f6d07b88e165194
Alexander Elfimow <my.free.mail@gmx.de>
2025-03-17 20:19:28 +0100

    formula44, fp128: added file

---
+++ formula44/fp128/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp128.h"
+#include "rhs.h"
+
+#include <quadmath.h>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \tan{\frac{\alpha}{2}} = \sqrt{\frac{1 - \cos{\alpha}}{1 + \cos{\alpha}}}
+ * @f]
+ */
+fp128 rhs(fp128 x)
+{
+    fp128 const a = cosq(x);
+
+    fp128 const result = sqrtq((1.0Q - a) / (1.0Q + a));
+
+    return result;
+}
