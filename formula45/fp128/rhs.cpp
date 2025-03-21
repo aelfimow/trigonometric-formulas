f93c5eaa1c0bca8de119e5fc9822c9e3e1d428b6
Alexander Elfimow <my.free.mail@gmx.de>
2025-03-21 20:26:59 +0100

    formula45, fp128: added file

---
+++ formula45/fp128/rhs.cpp
@@ -0,0 +1,18 @@
+#include "fp128.h"
+#include "rhs.h"
+
+#include <quadmath.h>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \frac{1 - \cos{\alpha}}{\sin{\alpha}}
+ * @f]
+ */
+fp128 rhs(fp128 x)
+{
+    fp128 const result = (1.0Q - cosq(x)) / sinq(x);
+
+    return result;
+}
