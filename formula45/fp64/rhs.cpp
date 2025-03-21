9e5ef41b0543c68868f899d8247a4ce53e10e57e
Alexander Elfimow <my.free.mail@gmx.de>
2025-03-21 20:28:13 +0100

    formula45, fp64: added file

---
+++ formula45/fp64/rhs.cpp
@@ -0,0 +1,18 @@
+#include "fp64.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \frac{1 - \cos{\alpha}}{\sin{\alpha}}
+ * @f]
+ */
+fp64 rhs(fp64 x)
+{
+    fp64 const result = (1.0 - cos(x)) / sin(x);
+
+    return result;
+}
