201ad635ba91c328e0c44fdc5c5111e3302d2bc9
Alexander Elfimow <my.free.mail@gmx.de>
2025-04-14 19:40:19 +0200

    formula48, fp32: added file

---
+++ formula48/fp32/rhs.cpp
@@ -0,0 +1,18 @@
+#include "fp32.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \frac{1 + \cos{\alpha}}{\sin{\alpha}}
+ * @f]
+ */
+fp32 rhs(fp32 x)
+{
+    fp32 const result = (1.0f + cosf(x)) / sinf(x);
+
+    return result;
+}
