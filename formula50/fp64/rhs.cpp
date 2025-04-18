55a652c26ee7dbac4bb0b531054fceca61c3281f
Alexander Elfimow <my.free.mail@gmx.de>
2025-04-18 10:37:39 +0200

    formula50, fp64: added file

---
+++ formula50/fp64/rhs.cpp
@@ -0,0 +1,18 @@
+#include "fp64.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \sqrt{\frac{1 - \cos{2\alpha}}{2}}
+ * @f]
+ */
+fp64 rhs(fp64 x)
+{
+    fp64 const result = sqrt((1.0 - cos(2.0 * x)) / 2.0);
+
+    return result;
+}
