0996d62011f1f3475995812dbcad2d1957428ffd
Alexander Elfimow <my.free.mail@gmx.de>
2025-04-18 10:14:42 +0200

    formula49, fp80: added file

---
+++ formula49/fp80/rhs.cpp
@@ -0,0 +1,18 @@
+#include "fp80.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \frac{\sin{\alpha}}{1 - \cos{\alpha}}
+ * @f]
+ */
+fp80 rhs(fp80 x)
+{
+    fp80 const result = sinl(x) / (1.0L - cosl(x));
+
+    return result;
+}
