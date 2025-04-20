ba6ebbb3e812f1c6ef2b81d126cbe9cb39a59ba9
Alexander Elfimow <my.free.mail@gmx.de>
2025-04-20 11:32:43 +0200

    formula51, fp80: added file

---
+++ formula51/fp80/rhs.cpp
@@ -0,0 +1,18 @@
+#include "fp80.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \sqrt{\frac{1 - \cos{2\alpha}}{2}}
+ * @f]
+ */
+fp80 rhs(fp80 x)
+{
+    fp80 const result = sqrtl((1.0L - cosl(2.0L * x)) / 2.0L);
+
+    return result;
+}
