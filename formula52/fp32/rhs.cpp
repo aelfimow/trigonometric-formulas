d2acbaa90c563986b144091a0366bc8cb060e856
Alexander Elfimow <my.free.mail@gmx.de>
2025-04-20 11:45:37 +0200

    formula52, fp32: added file

---
+++ formula52/fp32/rhs.cpp
@@ -0,0 +1,18 @@
+#include "fp32.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \sqrt{\frac{1 + \cos{2\alpha}}{2}}
+ * @f]
+ */
+fp32 rhs(fp32 x)
+{
+    fp32 const result = sqrtf((1.0f + cosf(2.0f * x)) / 2.0f);
+
+    return result;
+}
