c0d77620044dd197bce91180efc9321a6ba9e7af
Alexander Elfimow <my.free.mail@gmx.de>
2025-05-02 20:42:25 +0200

    formula55, fp128: added file

---
+++ formulas/formula55/fp128/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp128.h"
+#include "rhs.h"
+
+#include <quadmath.h>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \sqrt{\frac{1 + \cos{2\alpha}}{1 - \cos{2\alpha}}}
+ * @f]
+ */
+fp128 rhs(fp128 x)
+{
+    fp128 const a = cosq(2.0Q * x);
+
+    fp128 const result = sqrtq((1.0Q + a) / (1.0Q - a));
+
+    return result;
+}
