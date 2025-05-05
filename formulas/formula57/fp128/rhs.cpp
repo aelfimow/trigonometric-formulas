ec29f54b932fc13dcc05461d26a88fae5a20e99e
Alexander Elfimow <my.free.mail@gmx.de>
2025-05-05 18:12:02 +0200

    formula57, fp128: added file

---
+++ formulas/formula57/fp128/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp128.h"
+#include "rhs.h"
+
+#include <quadmath.h>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \frac{1 + \cos{2\alpha}}{\sin{2\alpha}}
+ * @f]
+ */
+fp128 rhs(fp128 x)
+{
+    fp128 const x2 = (2.0Q * x);
+
+    fp128 const result = (1.0Q + cosq(x2)) / sinq(x2);
+
+    return result;
+}
