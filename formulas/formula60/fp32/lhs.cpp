6fb03f3517071edf8b2bc3e83171e6bf6e78156f
Alexander Elfimow <my.free.mail@gmx.de>
2025-05-19 21:21:08 +0200

    formula60, fp32: added file

---
+++ formulas/formula60/fp32/lhs.cpp
@@ -0,0 +1,18 @@
+#include "fp32.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \sin{5\alpha}
+ * @f]
+ */
+fp32 lhs(fp32 x)
+{
+    fp32 const result = sinf(5.0f * x);
+
+    return result;
+}
