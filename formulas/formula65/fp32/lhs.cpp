d643859131f178fd3255ceb00b27b97f6f801ac6
Alexander Elfimow <my.free.mail@gmx.de>
2025-06-07 18:49:27 +0200

    formula65, fp32: added file

---
+++ formulas/formula65/fp32/lhs.cpp
@@ -0,0 +1,18 @@
+#include "fp32.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \tan{4\alpha}
+ * @f]
+ */
+fp32 lhs(fp32 x)
+{
+    fp32 const result = tanf(4.0f * x);
+
+    return result;
+}
