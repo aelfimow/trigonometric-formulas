8888b51b7ba99577f37fde18e8c7a3b1374f1deb
Alexander Elfimow <my.free.mail@gmx.de>
2025-08-31 20:40:18 +0200

    formula70, fp64: added file

---
+++ formulas/formula70/fp64/lhs.cpp
@@ -0,0 +1,21 @@
+#include "fp64.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \cos{\alpha} + \cos{\beta}
+ * @f]
+ */
+fp64 lhs(fp64 alpha, fp64 beta)
+{
+    fp64 const a = cos(alpha);
+    fp64 const b = cos(beta);
+
+    fp64 const result = (a + b);
+
+    return result;
+}
