12523b1bcb459e516412dd94ec4fcf64ee176e86
Alexander Elfimow <my.free.mail@gmx.de>
2025-09-24 19:58:24 +0200

    formula71, fp80: added file

---
+++ formulas/formula71/fp80/rhs.cpp
@@ -0,0 +1,21 @@
+#include "fp80.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      -2 \sin{\frac{\alpha + \beta}{2}} \sin{\frac{\alpha - \beta}{2}}
+ * @f]
+ */
+fp80 rhs(fp80 alpha, fp80 beta)
+{
+    fp80 const a = sinl((alpha + beta) / 2.0L);
+    fp80 const b = sinl((alpha - beta) / 2.0L);
+
+    fp80 const result = (-2.0L * a * b);
+
+    return result;
+}
