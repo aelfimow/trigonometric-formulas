21ece876c6023281fc814174df1af86a339a2f6b
Alexander Elfimow <my.free.mail@gmx.de>
2025-09-26 19:34:57 +0200

    formula72, fp80: added file

---
+++ formulas/formula72/fp80/lhs.cpp
@@ -0,0 +1,21 @@
+#include "fp80.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \tan{\alpha} + \tan{\beta}
+ * @f]
+ */
+fp80 lhs(fp80 alpha, fp80 beta)
+{
+    fp80 const a = tanl(alpha);
+    fp80 const b = tanl(beta);
+
+    fp80 const result = (a + b);
+
+    return result;
+}
