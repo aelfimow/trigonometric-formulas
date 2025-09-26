816e68deddebd7660c4e8ad14c393e65b2f5da54
Alexander Elfimow <my.free.mail@gmx.de>
2025-09-26 19:35:00 +0200

    formula72, fp80: added file

---
+++ formulas/formula72/fp80/rhs.cpp
@@ -0,0 +1,22 @@
+#include "fp80.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      \frac{\sin{(\alpha + \beta)}}{\cos{\alpha}\cos{\beta}}
+ * @f]
+ */
+fp80 rhs(fp80 alpha, fp80 beta)
+{
+    fp80 const s = sinl(alpha + beta);
+    fp80 const c1 = cosl(alpha);
+    fp80 const c2 = cosl(beta);
+
+    fp80 const result = (s / (c1 * c2));
+
+    return result;
+}
