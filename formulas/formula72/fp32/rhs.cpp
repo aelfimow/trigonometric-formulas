c0c1eb8b9e9b7eb9f73eeed3a0a70acb8d76409e
Alexander Elfimow <my.free.mail@gmx.de>
2025-09-26 19:34:45 +0200

    formula72, fp32: added file

---
+++ formulas/formula72/fp32/rhs.cpp
@@ -0,0 +1,22 @@
+#include "fp32.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      \frac{\sin{(\alpha + \beta)}}{\cos{\alpha}\cos{\beta}}
+ * @f]
+ */
+fp32 rhs(fp32 alpha, fp32 beta)
+{
+    fp32 const s = sinf(alpha + beta);
+    fp32 const c1 = cosf(alpha);
+    fp32 const c2 = cosf(beta);
+
+    fp32 const result = (s / (c1 * c2));
+
+    return result;
+}
