da1b4c761f3304cf36592c689e08c973fe20472a
Alexander Elfimow <my.free.mail@gmx.de>
2025-09-26 19:34:53 +0200

    formula72, fp64: added file

---
+++ formulas/formula72/fp64/rhs.cpp
@@ -0,0 +1,22 @@
+#include "fp64.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      \frac{\sin{(\alpha + \beta)}}{\cos{\alpha}\cos{\beta}}
+ * @f]
+ */
+fp64 rhs(fp64 alpha, fp64 beta)
+{
+    fp64 const s = sin(alpha + beta);
+    fp64 const c1 = cos(alpha);
+    fp64 const c2 = cos(beta);
+
+    fp64 const result = (s / (c1 * c2));
+
+    return result;
+}
