478e29eb3805bd94bb6ad25e76edf549580a9739
Alexander Elfimow <my.free.mail@gmx.de>
2025-09-26 19:46:45 +0200

    formula73, fp32: added file

---
+++ formulas/formula73/fp32/lhs.cpp
@@ -0,0 +1,21 @@
+#include "fp32.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \tan{\alpha} - \tan{\beta}
+ * @f]
+ */
+fp32 lhs(fp32 alpha, fp32 beta)
+{
+    fp32 const a = tanf(alpha);
+    fp32 const b = tanf(beta);
+
+    fp32 const result = (a - b);
+
+    return result;
+}
