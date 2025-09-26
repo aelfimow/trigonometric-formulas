15b427faaffc3350badad9aad9f75043ec9fda83
Alexander Elfimow <my.free.mail@gmx.de>
2025-09-26 19:46:40 +0200

    formula73, fp128: added file

---
+++ formulas/formula73/fp128/rhs.cpp
@@ -0,0 +1,22 @@
+#include "fp128.h"
+#include "rhs.h"
+
+#include <quadmath.h>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      \frac{\sin{(\alpha - \beta)}}{\cos{\alpha}\cos{\beta}}
+ * @f]
+ */
+fp128 rhs(fp128 alpha, fp128 beta)
+{
+    fp128 const s = sinq(alpha - beta);
+    fp128 const c1 = cosq(alpha);
+    fp128 const c2 = cosq(beta);
+
+    fp128 const result = (s / (c1 * c2));
+
+    return result;
+}
