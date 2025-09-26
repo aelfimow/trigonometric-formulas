d798fa33a619ceecc75c2385e5730e9421ae45fe
Alexander Elfimow <my.free.mail@gmx.de>
2025-09-26 19:46:36 +0200

    formula73, fp128: added file

---
+++ formulas/formula73/fp128/lhs.cpp
@@ -0,0 +1,21 @@
+#include "fp128.h"
+#include "lhs.h"
+
+#include <quadmath.h>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \tan{\alpha} - \tan{\beta}
+ * @f]
+ */
+fp128 lhs(fp128 alpha, fp128 beta)
+{
+    fp128 const a = tanq(alpha);
+    fp128 const b = tanq(beta);
+
+    fp128 const result = (a - b);
+
+    return result;
+}
