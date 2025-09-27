3e919c2b2b437fbebc83dd531453a6f029b22f4b
Alexander Elfimow <my.free.mail@gmx.de>
2025-09-27 22:13:05 +0200

    formula74, fp32: fixed lhs

--- formulas/formula74/fp32/lhs.cpp
+++ formulas/formula74/fp32/lhs.cpp
@@ -14,7 +14,7 @@ fp32 lhs(fp32 alpha, fp32 beta)
     fp32 const a = cotf(alpha);
     fp32 const b = cotf(beta);
 
-    fp32 const result = (a - b);
+    fp32 const result = (a + b);
 
     return result;
 }
