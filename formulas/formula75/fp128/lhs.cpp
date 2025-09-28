f98d3d211dfbd247964cadf695f030c38a4c585b
Alexander Elfimow <my.free.mail@gmx.de>
2025-09-28 19:33:38 +0200

    formula75, fp128: added file

---
+++ formulas/formula75/fp128/lhs.cpp
@@ -0,0 +1,20 @@
+#include "fp128.h"
+#include "lhs.h"
+#include "cotq.h"
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \cot{\alpha} - \cot{\beta}
+ * @f]
+ */
+fp128 lhs(fp128 alpha, fp128 beta)
+{
+    fp128 const a = cotq(alpha);
+    fp128 const b = cotq(beta);
+
+    fp128 const result = (a - b);
+
+    return result;
+}
