5c50835e1017fc18632749032d67561c440af786
Alexander Elfimow <my.free.mail@gmx.de>
2025-10-04 20:41:10 +0200

    formula76, fp64: added file

---
+++ formulas/formula76/fp64/rhs.cpp
@@ -0,0 +1,23 @@
+#include "fp64.h"
+#include "rhs.h"
+
+#include <numbers>
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \sqrt{2} \sin{(45^\circ + \alpha)}
+ * @f]
+ */
+fp64 rhs(fp64 x)
+{
+    fp64 const circ_45 = (std::numbers::pi_v<fp64> / 4.0);
+
+    fp64 const s = sin(circ_45 + x);
+
+    fp64 const result = (sqrt(2.0) * s);
+
+    return result;
+}
