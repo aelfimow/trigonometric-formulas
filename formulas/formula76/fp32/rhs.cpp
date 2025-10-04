0a4ea851b83db92811109ea37a5926200aa254b2
Alexander Elfimow <my.free.mail@gmx.de>
2025-10-04 20:41:07 +0200

    formula76, fp32: added file

---
+++ formulas/formula76/fp32/rhs.cpp
@@ -0,0 +1,23 @@
+#include "fp32.h"
+#include "rhs.h"
+
+#include <numbers>
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \sqrt{2} \sin{(45^\circ + \alpha)}
+ * @f]
+ */
+fp32 rhs(fp32 x)
+{
+    fp32 const circ_45 = (std::numbers::pi_v<fp32> / 4.0f);
+
+    fp32 const s = sinf(circ_45 + x);
+
+    fp32 const result = (sqrtf(2.0f) * s);
+
+    return result;
+}
