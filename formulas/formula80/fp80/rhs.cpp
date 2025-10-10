f0290001ea2eae03a689344decd2050b6f4996d3
Alexander Elfimow <my.free.mail@gmx.de>
2025-10-10 20:37:18 +0200

    formula80, fp80: added file

---
+++ formulas/formula80/fp80/rhs.cpp
@@ -0,0 +1,23 @@
+#include "fp80.h"
+#include "rhs.h"
+
+#include <numbers>
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \sqrt{2} \cos{(45^\circ + \alpha)}
+ * @f]
+ */
+fp80 rhs(fp80 x)
+{
+    fp80 const circ_45 = (std::numbers::pi_v<fp80> / 4.0L);
+
+    fp80 const s = cosl(circ_45 + x);
+
+    fp80 const result = (sqrtl(2.0) * s);
+
+    return result;
+}
