e6cd2987b221188688a98b7a50d219b1d6a9e6b9
Alexander Elfimow <my.free.mail@gmx.de>
2025-10-10 20:37:17 +0200

    formula80, fp80: added file

---
+++ formulas/formula80/fp80/lhs.cpp
@@ -0,0 +1,21 @@
+#include "fp80.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \cos{\alpha} - \sin{\alpha}
+ * @f]
+ */
+fp80 lhs(fp80 x)
+{
+    fp80 const c = cosl(x);
+    fp80 const s = sinl(x);
+
+    fp80 const result = (c - s);
+
+    return result;
+}
