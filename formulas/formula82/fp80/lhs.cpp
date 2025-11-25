43bf7acf2e6930ae82384cb315af5a4e6cc85ddb
Alexander Elfimow <my.free.mail@gmx.de>
2025-11-25 19:47:10 +0100

    formula82, fp80: added file

---
+++ formulas/formula82/fp80/lhs.cpp
@@ -0,0 +1,21 @@
+#include "fp80.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \sin{\alpha} \sin{\beta}
+ * @f]
+ */
+fp80 lhs(fp80 alpha, fp80 beta)
+{
+    fp80 const a = sinl(alpha);
+    fp80 const b = sinl(beta);
+
+    fp80 const result = (a * b);
+
+    return result;
+}
