333a8464d26430c623b4d3e4c4a6152c4397234f
Alexander Elfimow <my.free.mail@gmx.de>
2025-11-25 19:46:51 +0100

    formula82, fp128: added file

---
+++ formulas/formula82/fp128/rhs.cpp
@@ -0,0 +1,21 @@
+#include "fp128.h"
+#include "rhs.h"
+
+#include <quadmath.h>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      \frac{1}{2} (\cos{(\alpha - \beta)} - \cos{(\alpha + \beta)})
+ * @f]
+ */
+fp128 rhs(fp128 alpha, fp128 beta)
+{
+    fp128 const a = cosq(alpha - beta);
+    fp128 const b = cosq(alpha + beta);
+
+    fp128 const result = (1.0Q / 2.0Q) * (a - b);
+
+    return result;
+}
