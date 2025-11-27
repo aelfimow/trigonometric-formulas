db0c374b283d6fbf425089b30538825b47d9d177
Alexander Elfimow <my.free.mail@gmx.de>
2025-11-27 19:12:09 +0100

    formula84, fp32: added file

---
+++ formulas/formula84/fp32/rhs.cpp
@@ -0,0 +1,21 @@
+#include "fp32.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      \frac{1}{2} (\sin{(\alpha + \beta)} + \sin{(\alpha - \beta)})
+ * @f]
+ */
+fp32 rhs(fp32 alpha, fp32 beta)
+{
+    fp32 const a = sinf(alpha + beta);
+    fp32 const b = sinf(alpha - beta);
+
+    fp32 const result = (1.0f / 2.0f) * (a + b);
+
+    return result;
+}
