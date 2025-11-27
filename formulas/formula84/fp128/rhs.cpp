900b469182330ff4849f491203ca27fc1cbfe378
Alexander Elfimow <my.free.mail@gmx.de>
2025-11-27 19:12:05 +0100

    formula84, fp128: added file

---
+++ formulas/formula84/fp128/rhs.cpp
@@ -0,0 +1,21 @@
+#include "fp128.h"
+#include "rhs.h"
+
+#include <quadmath.h>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      \frac{1}{2} (\sin{(\alpha + \beta)} + \sin{(\alpha - \beta)})
+ * @f]
+ */
+fp128 rhs(fp128 alpha, fp128 beta)
+{
+    fp128 const a = sinq(alpha + beta);
+    fp128 const b = sinq(alpha - beta);
+
+    fp128 const result = (1.0Q / 2.0Q) * (a + b);
+
+    return result;
+}
