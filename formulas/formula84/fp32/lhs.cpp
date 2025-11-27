6eb8978f0e728b9fcbf67a5a05f0d1e3bdbf4df1
Alexander Elfimow <my.free.mail@gmx.de>
2025-11-27 19:12:07 +0100

    formula84, fp32: added file

---
+++ formulas/formula84/fp32/lhs.cpp
@@ -0,0 +1,21 @@
+#include "fp32.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \sin{\alpha} \cos{\beta}
+ * @f]
+ */
+fp32 lhs(fp32 alpha, fp32 beta)
+{
+    fp32 const a = sinf(alpha);
+    fp32 const b = cosf(beta);
+
+    fp32 const result = (a * b);
+
+    return result;
+}
