66fa60a0b8a6a052f2d55f1c240d062eb8c3929f
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-07 12:14:32 +0100

    formula88, fp80: added file

---
+++ formulas/formula88/fp80/rhs.cpp
@@ -0,0 +1,24 @@
+#include "fp80.h"
+#include "rhs.h"
+#include "cotl.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      \frac{\cot{\alpha} + \cot{\beta}}{\tan{\alpha} + \tan{\beta}}
+ * @f]
+ */
+fp80 rhs(fp80 alpha, fp80 beta)
+{
+    fp80 const a = tanl(alpha);
+    fp80 const b = tanl(beta);
+    fp80 const ca = cotl(alpha);
+    fp80 const cb = cotl(beta);
+
+    fp80 const result = (ca + cb) / (a + b);
+
+    return result;
+}
