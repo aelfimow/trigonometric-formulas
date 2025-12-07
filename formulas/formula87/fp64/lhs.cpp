e51fa7c7b356b3d35a2979290b79cf13187877a2
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-07 11:59:22 +0100

    formula87, fp64: added file

---
+++ formulas/formula87/fp64/lhs.cpp
@@ -0,0 +1,21 @@
+#include "fp64.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \tan{\alpha} \tan{\beta}
+ * @f]
+ */
+fp64 lhs(fp64 alpha, fp64 beta)
+{
+    fp64 const a = tan(alpha);
+    fp64 const b = tan(beta);
+
+    fp64 const result = (a * b);
+
+    return result;
+}
