a16ba9a799a7c4a81fb8706dbe0a8fd3b5d5a84c
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-07 12:14:05 +0100

    formula88, fp128: added file

---
+++ formulas/formula88/fp128/rhs.cpp
@@ -0,0 +1,24 @@
+#include "fp128.h"
+#include "rhs.h"
+#include "cotq.h"
+
+#include <quadmath.h>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      \frac{\cot{\alpha} + \cot{\beta}}{\tan{\alpha} + \tan{\beta}}
+ * @f]
+ */
+fp128 rhs(fp128 alpha, fp128 beta)
+{
+    fp128 const a = tanq(alpha);
+    fp128 const b = tanq(beta);
+    fp128 const ca = cotq(alpha);
+    fp128 const cb = cotq(beta);
+
+    fp128 const result = (ca + cb) / (a + b);
+
+    return result;
+}
