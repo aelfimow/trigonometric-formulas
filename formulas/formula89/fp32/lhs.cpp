e303ec579efa872758a70745557bc2463818ae7b
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-12 19:46:27 +0100

    formula89, fp32: added file

---
+++ formulas/formula89/fp32/lhs.cpp
@@ -0,0 +1,20 @@
+#include "fp32.h"
+#include "lhs.h"
+#include "cotf.h"
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \cot{\alpha} \cot{\beta}
+ * @f]
+ */
+fp32 lhs(fp32 alpha, fp32 beta)
+{
+    fp32 const a = cotf(alpha);
+    fp32 const b = cotf(beta);
+
+    fp32 const result = (a * b);
+
+    return result;
+}
