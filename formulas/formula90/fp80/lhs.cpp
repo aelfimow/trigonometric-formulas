5d6970ca1bceec20ee9adfcf58417ed2998d492a
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-13 21:40:04 +0100

    formula90, fp80: added file

---
+++ formulas/formula90/fp80/lhs.cpp
@@ -0,0 +1,22 @@
+#include "fp80.h"
+#include "lhs.h"
+#include "cotl.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \tan{\alpha} \cot{\beta}
+ * @f]
+ */
+fp80 lhs(fp80 alpha, fp80 beta)
+{
+    fp80 const a = tanl(alpha);
+    fp80 const b = cotl(beta);
+
+    fp80 const result = (a * b);
+
+    return result;
+}
