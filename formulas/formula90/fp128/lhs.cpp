d22d88918fb67ce534e86c6f986caf38b5005747
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-13 21:39:52 +0100

    formula90, fp128: added file

---
+++ formulas/formula90/fp128/lhs.cpp
@@ -0,0 +1,22 @@
+#include "fp128.h"
+#include "lhs.h"
+#include "cotq.h"
+
+#include <quadmath.h>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \tan{\alpha} \cot{\beta}
+ * @f]
+ */
+fp128 lhs(fp128 alpha, fp128 beta)
+{
+    fp128 const a = tanq(alpha);
+    fp128 const b = cotq(beta);
+
+    fp128 const result = (a * b);
+
+    return result;
+}
