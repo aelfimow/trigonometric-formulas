f73d6a71abe47d26a4bf51d906e0a764eca80b13
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-26 16:27:42 +0100

    formula93, fp128: added file

---
+++ formulas/formula93/fp128/rhs.cpp
@@ -0,0 +1,24 @@
+#include "fp128.h"
+#include "rhs.h"
+#include "cotq.h"
+
+#include <quadmath.h>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      -\frac{\cot{\alpha} - \tan{\beta}}{\tan{\alpha} - \cot{\beta}}
+ * @f]
+ */
+fp128 rhs(fp128 alpha, fp128 beta)
+{
+    fp128 const ta = tanq(alpha);
+    fp128 const tb = tanq(beta);
+    fp128 const ca = cotq(alpha);
+    fp128 const cb = cotq(beta);
+
+    fp128 const result = -(ca - tb) / (ta - cb);
+
+    return result;
+}
