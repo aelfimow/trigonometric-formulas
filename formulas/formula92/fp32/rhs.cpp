92d7e0c24bcb4b6ee042f6e657cc990e6716d5bd
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-26 12:57:32 +0100

    formula92, fp32: added file

---
+++ formulas/formula92/fp32/rhs.cpp
@@ -0,0 +1,24 @@
+#include "fp32.h"
+#include "rhs.h"
+#include "cotf.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      \frac{\cot{\alpha} + \tan{\beta}}{\tan{\alpha} + \cot{\beta}}
+ * @f]
+ */
+fp32 rhs(fp32 alpha, fp32 beta)
+{
+    fp32 const ta = tanf(alpha);
+    fp32 const tb = tanf(beta);
+    fp32 const ca = cotf(alpha);
+    fp32 const cb = cotf(beta);
+
+    fp32 const result = (ca + tb) / (ta + cb);
+
+    return result;
+}
