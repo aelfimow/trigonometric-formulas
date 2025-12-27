5103829638f6434f678816094bcef903f534f285
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-27 17:49:53 +0100

    formula94, fp80: added file

---
+++ formulas/formula94/fp80/rhs.cpp
@@ -0,0 +1,23 @@
+#include "fp80.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      \frac{1}{4} (\sin{(\alpha + \beta - \gamma)} + \sin{(\beta + \gamma - \alpha)} + \sin{(\gamma + \alpha - \beta)} - \sin{(\alpha + \beta + \gamma)})
+ * @f]
+ */
+fp80 rhs(fp80 alpha, fp80 beta, fp80 gamma)
+{
+    fp80 const a = sinl(alpha + beta - gamma);
+    fp80 const b = sinl(beta + gamma - alpha);
+    fp80 const c = sinl(gamma + alpha - beta);
+    fp80 const d = sinl(alpha + beta + gamma);
+
+    fp80 const result = (1.0L / 4.0L) * (a + b + c - d);
+
+    return result;
+}
