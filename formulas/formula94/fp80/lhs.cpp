b7d8f4fb7ce12bd4395bb223e1ee135f6d946e95
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-27 17:49:51 +0100

    formula94, fp80: added file

---
+++ formulas/formula94/fp80/lhs.cpp
@@ -0,0 +1,22 @@
+#include "fp80.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \sin{\alpha} \sin{\beta} \sin{\gamma}
+ * @f]
+ */
+fp80 lhs(fp80 alpha, fp80 beta, fp80 gamma)
+{
+    fp80 const a = sinl(alpha);
+    fp80 const b = sinl(beta);
+    fp80 const g = sinl(gamma);
+
+    fp80 const result = (a * b * g);
+
+    return result;
+}
