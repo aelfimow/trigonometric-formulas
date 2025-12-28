0a07c6778e263163de72561d2c9ecc016dbca750
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-28 19:45:08 +0100

    formula97, fp32: added file

---
+++ formulas/formula97/fp32/lhs.cpp
@@ -0,0 +1,22 @@
+#include "fp32.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \sin{\alpha} \cos{\beta} \cos{\gamma}
+ * @f]
+ */
+fp32 lhs(fp32 alpha, fp32 beta, fp32 gamma)
+{
+    fp32 const a = sinf(alpha);
+    fp32 const b = cosf(beta);
+    fp32 const g = cosf(gamma);
+
+    fp32 const result = (a * b * g);
+
+    return result;
+}
