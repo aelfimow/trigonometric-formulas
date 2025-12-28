d53839ab967a273ae38e4454a86f010497f128bf
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-28 12:06:10 +0100

    formula95, fp80: added file

---
+++ formulas/formula95/fp80/lhs.cpp
@@ -0,0 +1,22 @@
+#include "fp80.h"
+#include "lhs.h"
+
+#include <cmath>
+
+/**
+ * Left Hand Side:
+ *
+ * @f[
+ *     \cos{\alpha} \cos{\beta} \cos{\gamma}
+ * @f]
+ */
+fp80 lhs(fp80 alpha, fp80 beta, fp80 gamma)
+{
+    fp80 const a = cosl(alpha);
+    fp80 const b = cosl(beta);
+    fp80 const g = cosl(gamma);
+
+    fp80 const result = (a * b * g);
+
+    return result;
+}
