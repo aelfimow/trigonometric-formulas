40ece7a96071460e392ecf6bfc7e58f72e37d47c
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-28 12:05:57 +0100

    formula95, fp32: added file

---
+++ formulas/formula95/fp32/rhs.cpp
@@ -0,0 +1,23 @@
+#include "fp32.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *      \frac{1}{4} (\cos{(\alpha + \beta - \gamma)} + \cos{(\beta + \gamma - \alpha)} + \cos{(\gamma + \alpha - \beta)} + \cos{(\alpha + \beta + \gamma)})
+ * @f]
+ */
+fp32 rhs(fp32 alpha, fp32 beta, fp32 gamma)
+{
+    fp32 const a = cosf(alpha + beta - gamma);
+    fp32 const b = cosf(beta + gamma - alpha);
+    fp32 const c = cosf(gamma + alpha - beta);
+    fp32 const d = cosf(alpha + beta + gamma);
+
+    fp32 const result = (1.0f / 4.0f) * (a + b + c + d);
+
+    return result;
+}
