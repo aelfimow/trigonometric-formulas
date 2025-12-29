45bbc1e61b6498b40971d87c4c79d7a55b48ca5f
Alexander Elfimow <my.free.mail@gmx.de>
2025-12-29 22:11:27 +0100

    formula98, fp80: added file

---
+++ formulas/formula98/fp80/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp80.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \frac{1}{2}(1 - \cos{2\alpha})
+ * @f]
+ */
+fp80 rhs(fp80 x)
+{
+    fp80 const a = cosl(2.0L * x);
+
+    fp80 const result = (1.0L / 2.0L) * (1.0L - a);
+
+    return result;
+}
